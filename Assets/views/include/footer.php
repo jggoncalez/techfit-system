86ad8deae3941f8cc35a1e36fb03571239645883
jggoncalez <joaogabriel.racer@gmail.com>
2025-10-09 16:54:35 -0300

    feat: add header, footer, and academies data files

---
+++ Assets/views/include/footer.php
@@ -0,0 +1,28 @@
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center"> <a href="/"
+                class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> <svg
+                    class="bi" width="30" height="24" aria-hidden="true">
+                    <use xlink:href="#bootstrap"></use>
+                </svg> </a> <span class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span> </div>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Instagram"><svg class="bi" width="24"
+                        height="24" aria-hidden="true">
+                        <use xlink:href="#instagram"></use>
+                    </svg></a></li>
+            <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Facebook"><svg class="bi" width="24"
+                        height="24">
+                        <use xlink:href="#facebook"></use>
+                    </svg></a></li>
+        </ul>
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,67 +1,11 @@
-<!doctype html>
-<html lang="pt-br">
-
-<head>
-    <title>TechFit</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="shortcut icon" href="/TechFit/Código/Assets/images/TechFit-icon.ico" type="image/x-icon">
-
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" /> -->
-    <script src="/docs/5.3/assets/js/color-modes.js"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <header>
-        <div class="px-3 py-2 text-bg-light border-bottom">
-            <div class="container">
-                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"> <a
-                        href="/"
-                        class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-black text-decoration-none"> <img
-                            class="bi me-2" width="50%" height="50%" src="logo-fixed.webp"></img> </a>
-                    <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-                        <li> <a href="#" class="nav-link text-secondary"> <svg class="bi d-block mx-auto mb-1"
-                                    width="24" height="24" aria-hidden="true">
-                                    <use xlink:href="#home"></use>
-                                </svg>
-                                Home
-                            </a> </li>
-                        <li> <a href="#" class="nav-link text-black"> <svg class="bi d-block mx-auto mb-1" width="24"
-                                    height="24" aria-hidden="true">
-                                    <use xlink:href="#speedometer2"></use>
-                                </svg>
-                                Sobre Nóssss
-                            </a> </li>
-                        <li> <a href="#" class="nav-link text-black"> <svg class="bi d-block mx-auto mb-1" width="24"
-                                    height="24" aria-hidden="true">
-                                    <use xlink:href="#table"></use>
-                                </svg>
-                                Sou Cliente
-                            </a> </li>
-                        <li> <a href="#" class="nav-link text-black"> <svg class="bi d-block mx-auto mb-1" width="24"
-                                    height="24" aria-hidden="true">
-                                    <use xlink:href="#grid"></use>
-                                </svg>
-                                Sou Funcionário
-                            </a> </li>
-                        <li> <a href="#" class="nav-link text-black"> <svg class="bi d-block mx-auto mb-1" width="24"
-                                    height="24" aria-hidden="true">
-                                    <use xlink:href="#people-circle"></use>
-                                </svg>
-                                Customers
-                            </a> </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </header>
+<?php
+include_once 'Assets/views/include/header.php';
+?>
     <main>
         <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
             <div class="col-md-6 p-lg-5 mx-auto my-5">
                 <h3 class="fw-normal text-muted mb-3">A tecnologia que treina com você</h3>
-                <h1 class="display-3 fw-bold">Bem Vindo A Tech Fit oioi </h1>
+                <h1 class="display-3 fw-bold">Bem Vindo A Tech Fit</h1>
                 <div class="d-flex gap-3 justify-content-center lead fw-normal"> <a class="icon-link" href="#">
                         Seja um Cliente
                         <svg class="bi" aria-hidden="true">
@@ -77,9 +21,9 @@
         </div>
         <div class="sobre">
         <div class="row featurette">
-            <div class="col-md-7">
-                <h2 class="featurette-heading fw-normal lh-1">Sobre a TechFit </h2>
-                <p class="lead">A Techfit une tecnologia e saúde para transformar a forma de treinar. Com o SAGEF – Sistema Automatizado de Geração de Exercícios Físicos, criamos treinos personalizados a partir dos dados e objetivos de cada usuário, ajustando séries, repetições e cargas conforme o feedback. Assim, garantimos evolução contínua, segurança e motivação em cada etapa.</p>
+            <div class="col-md-7 align-items-center justify-content-center">
+                <h2 class="featurette-heading fw-normal lh-1 ">Sobre a TechFit </h2>
+                <p class="lead  ">A Techfit une tecnologia e saúde para transformar a forma de treinar. Com o SAGEF – Sistema Automatizado de Geração de Exercícios Físicos, criamos treinos personalizados a partir dos dados e objetivos de cada usuário, ajustando séries, repetições e cargas conforme o feedback. Assim, garantimos evolução contínua, segurança e motivação em cada etapa.</p>
             </div>
             <div class="col-md-5"> <svg aria-label="Placeholder: 500x500"
                     class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" height="500"
@@ -90,9 +34,9 @@
                 </svg> </div>
         </div>
         </div>
-        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="row row-cols-1 row-cols-md-6 mb-3 text-center align-items-center justify-content-center">
             <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card mb-1 rounded-3 shadow-sm">
                     <div class="card-header py-3">
                         <h4 class="my-0 fw-normal">Free</h4>
                     </div>
@@ -109,7 +53,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-1">
+            <div class="col">
                 <div class="card mb-1 rounded-3 shadow-sm">
                     <div class="card-header py-3">
                         <h4 class="my-0 fw-normal">Pro</h4>
@@ -127,7 +71,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                <div class="card mb- rounded-3 shadow-sm border-primary">
                     <div class="card-header py-3 text-bg-primary border-primary">
                         <h4 class="my-0 fw-normal">Enterprise</h4>
                     </div>
@@ -175,31 +119,7 @@
         </div>
         </div>
     </main>
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center"> <a href="/"
-                class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> <svg
-                    class="bi" width="30" height="24" aria-hidden="true">
-                    <use xlink:href="#bootstrap"></use>
-                </svg> </a> <span class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span> </div>
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Instagram"><svg class="bi" width="24"
-                        height="24" aria-hidden="true">
-                        <use xlink:href="#instagram"></use>
-                    </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Facebook"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#facebook"></use>
-                    </svg></a></li>
-        </ul>
-    </footer>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+require_once 'Assets/views/include/footer.php';
+?>
