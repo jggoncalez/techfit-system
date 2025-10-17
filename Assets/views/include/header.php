889054c2d1a30bfcee113ff07b9c33da6c4e914f
Henrique-RMotta <henrique2008790@gmail.com>
2025-10-17 16:41:31 -0300

    Fix header and feat: database

--- Assets/views/include/header.php
+++ Assets/views/include/header.php
@@ -21,7 +21,7 @@
                         class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-black text-decoration-none"> <img
                             class="bi me-2" width="50%" height="50%" src="Assets\images\logo-fixed.webp"></img> </a>
                     <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-                        <li> <a href="#" class="nav-link text-secondary"> <svg class="bi d-block mx-auto mb-1"
+                        <li> <a href="#" class="nav-link text-black"> <svg class="bi d-block mx-auto mb-1"
                                     width="24" height="24" aria-hidden="true">
                                     <use xlink:href="#home"></use>
                                 </svg>
