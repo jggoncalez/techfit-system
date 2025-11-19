93d595e1a823b8f67d27c32ce3c5cac96af71a63
jggoncalez <joaogabriel.racer@gmail.com>
2025-11-18 22:08:33 -0300

    feat: add user-related views for main, profile, schedule, and training

---
+++ views/usuario/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>Placeholder</header>
+
+    <h2>Seja bem-vindo <p id="user_name">User</p>!</h2>
+</body>
+</html>
---
+++ views/usuario/profile.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>Placeholder</header>
+
+    <main>
+        <div class="plan">
+            <p><b>Plano: </b> <p id="user-plan">?</p></p>
+        </div>
+        <div class="pf-container">
+            <img src="https://placehold.co/50x50" alt="Placeholder">
+            <p><b>Nome:</b> <p id="full-name"></p></p>
+            <p><b>Gênero:</b> <p id="gender"></p></p>
+            <p><b>Idade:</b> <p id="age"></p></p>
+            <p><b>Endereço:</b> <p id="address"></p></p>
+            <p><b>Data de Nascimento:</b> <p id="birth-date"></p></p>
+        </div>
+
+        <div class="body-info-container">
+            <p><b>Peso:</b> <p id="weight"></p></p>
+            <p><b>Altura:</b> <p id="height"></p></p>
+            <p><b>% Gordura:</b> <p id="body-fat"></p></p>
+            <p><b>Objetivo:</b> <p id="goal"></p></p>
+            <p><b>Já treinou antes:</b> <p id="training-experience"></p></p>
+        </div>
+
+        <div class="availability-container">
+            <h3>Disponibilidade</h3>
+            <table class="availability-table">
+            <thead>
+                <tr>
+                <th>Dia</th>
+                <th>Disponível</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Segunda</td>
+                <td></td>
+                </tr>
+                <tr>
+                <td>Terça</td>
+                <td></td>
+                </tr>
+                <tr>
+                <td>Quarta</td>
+                <td></td>
+                </tr>
+                <tr>
+                <td>Quinta</td>
+                <td></td>
+                </tr>
+                <tr>
+                <td>Sexta</td>
+                <td></td>
+                </tr>
+                <tr>
+                <td>Sábado</td>
+                <td></td>
+                </tr>
+                <tr>
+                <td>Domingo</td>
+                <td></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
