02f94ac8df55d5e28e9a5bd141ff13519ce6bbee
jggoncalez <joaogabriel.racer@gmail.com>
2025-12-08 16:55:40 -0300

    fix: user sidebar (O_O)

--- views/usuario/user-schedule.php
+++ views/usuario/user-schedule.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="stylesheet" href="../../Assets/style/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 
 </html>
@@ -87,44 +88,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <div class="d-flex" style="height: 100vh; overflow-y: auto;">
         <!-- Sidebar -->
-        <div class="sidebar d-flex flex-column flex-shrink-0 p-3 bg-light">
+        <div class="sidebar d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                 <img src="../../public/images/logo-fixed.webp" class="img-fluid mb-2" alt="TechFit Logo" style="max-width: 150px;">
             </a>
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="/usuario" class="nav-link link-dark">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="/usuario/profile" class="nav-link link-dark">
-                        Meu Perfil
+                    <a href="/usuario" class="nav-link link-dark" >
+                        <i class="bi bi-house-door me-2"></i>Home
                     </a>
                 </li>
                 <li>
                     <a href="/usuario/user/training" class="nav-link link-dark">
-                        Meus Treinos
+                        <i class="bi bi-book me-2"></i>Meus Treinos
                     </a>
                 </li>
                 <li>
-                    <a href="/usuario/user/schedule" class="nav-link active" style="background-color: #e35c38;" aria-current="page">
-                        Meus agendamentos
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link link-dark">
-                        Sair
+                    <a href="/usuario/user/schedule" class="nav-link active text-white" style="background-color: #e35c38;" aria-current="page">
+                        <i class="bi bi-calendar-event me-2"></i>Meus agendamentos
                     </a>
                 </li>
             </ul>
             <hr>
             <div class="dropdown">
-                <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="../../public/images/pfp_placeholder.webp" alt="" width="32" height="32" class="rounded-circle me-2">
-                    <p id="user-name"><strong><?php echo $controller->US_NOME; ?></strong></p>
+                <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" 
+                   id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://placehold.co/32x32" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <p id="user-name"><strong><?php echo $controller->US_NOME ?></strong></p>
                 </a>
+                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                    <li><a class="dropdown-item" href="/usuario/profile"><i class="bi bi-person me-2"></i>Perfil</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right me-2"></i>Sair</a></li>
+                </ul>
             </div>
         </div>
 
